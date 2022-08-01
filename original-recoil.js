ea9df89992bc406648ef04432ac7cd6326595601
uhyo <uhyo@babel.jp>
2022-08-01 22:24:13 +0900

    find a way that work nicely

--- original-recoil.js
+++ original-recoil.js
@@ -1,4 +1,12 @@
-// Intermediate CJS file to load the original recoil library
+const path = require("path");
+const fs = require("fs");
 
-const recoilPath = require.resolve("recoil");
-module.exports = require(recoilPath);
+// Intermediate CJS file to load the original recoil library.
+
+if (fs.existsSync(path.join(__dirname, "node_modules/recoil"))) {
+  module.exports = require(path.join(__dirname, "node_modules/recoil"));
+} else {
+  // Currently this assumes that this module and the recoil library are in the same directory.
+  const recoilPath = path.join(__dirname, "../../recoil");
+  module.exports = require(recoilPath);
+}
